ba8ae9964cbfd57140da891f74ab403f1fd8b8b4
jam3s13 <jean.carlos.aj.13@gmail.com>
2026-06-09 15:25:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ class HeartParticle {
         this.opacity = Math.sin(Date.now() * 0.001 + this.t) * 0.5 + 0.5;
     }
     draw() {
-        ctx.fillStyle = `rgba(255, 20, 147, ${this.opacity})`;
+        ctx.fillStyle = `rgba(255, 0, 0, ${this.opacity})`;
         ctx.shadowBlur = 8; ctx.shadowColor = "#DC143C";
         ctx.beginPath(); ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill(); ctx.shadowBlur = 0;
